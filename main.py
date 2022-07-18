a15b3688fc2e71b70628a8e53f6d7d27288833b0
Rustam Isaev <eruvis@mail.ru>
2022-07-18 22:26:37 +0700

    minor edits args

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from ytvideouploader import YTVideoUploader
 from typing import Optional
 
 
-def run(video_path: str, video_title: Optional[str] = str):
-    assert YTVideoUploader(video_path, video_title).upload_video()
+def run(video_path: str, video_title: Optional[str] = str, headless: bool = False, fullscreen: bool = True):
+    assert YTVideoUploader(video_path, video_title, headless, fullscreen).upload_video()
 
 
 if __name__ == "__main__":
@@ -15,4 +15,4 @@ if __name__ == "__main__":
     parser.add_argument("--headless", type=bool, default=False)
     parser.add_argument("--fullscreen", type=bool, default=True)
     args = parser.parse_args()
-    run(args.video, args.title)
+    run(args.video, args.title, args.headless, args.fullscreen)
